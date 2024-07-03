0460526f76a877ae154520aec57e240297def919
shahdAls <128863844+shahdAls@users.noreply.github.com>
2024-07-03 22:56:16 +0300

    ff

--- app/Http/Controllers/Api/profileController.php
+++ app/Http/Controllers/Api/profileController.php
@@ -11,49 +11,49 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
-class profileController extends Controller {
+class ProfileController extends Controller
+{
 
-    public function viewProfile(Request $request) {
+    public function viewProfile(Request $request)
+    {
         $user = Auth::user();
 
         return response()->json(new UserResource($user), 200);
     }
 
     public function editProfile(Request $request)
-{
-    // Get the authenticated user
-    $user = Auth::user();
+    {
+        // Get the authenticated user
+        $user = Auth::user();
 
-    // Check if the user is authorized
-    if (!$user) {
-        return response()->json([
-            'error' => 'Unauthorized'
-        ], 403);
+        // Check if the user is authorized
+        if (!$user) {
+            return response()->json([
+                'error' => 'Unauthorized'
+            ], 403);
+        }
+
+        $userId = Auth::user()->id; // Get authenticated user's ID
+        $user = User::find($userId);
+
+        // Validate the incoming request data
+        $validatedData = $request->validate([
+            'name' => 'string|max:255',
+            'email' => 'string|email|max:255',
+            'bio' => 'nullable|string',
+            'pic' => 'nullable|string',
+        ]);
+
+        // Update user attributes with validated data
+        $user->update([
+            'name' => $validatedData['name'] ?? $user->name,
+            'email' => $validatedData['email'] ?? $user->email,
+            // Only set 'bio' if it exists in the request, even if it is null
+            'bio' => array_key_exists('bio', $validatedData) ? $validatedData['bio'] : $user->bio,
+            'pic' => $validatedData['pic'] ?? $user->pic
+        ]);
+
+
+        return response()->json(new UserResource($user), 200);
     }
-
-    $userId = Auth::user()->id; // Get authenticated user's ID
-    $user = User::find($userId);
-
-    // Validate the incoming request data
-    $validatedData = $request->validate([
-        'name' => 'string|max:255',
-        'email' => 'string|email|max:255',
-        'bio' => 'nullable|string',
-        'pic' => 'nullable|string',
-    ]);
-
-    // Update user attributes with validated data
-    $user->update([
-        'name' => $validatedData['name'] ?? $user->name,
-        'email' => $validatedData['email'] ?? $user->email,
-        // Only set 'bio' if it exists in the request, even if it is null
-        'bio' => array_key_exists('bio', $validatedData) ? $validatedData['bio'] : $user->bio,
-        'pic' => $validatedData['pic'] ?? $user->pic
-    ]);
-
-
-    return response()->json(new UserResource($user), 200);
-
-
-}
 }
